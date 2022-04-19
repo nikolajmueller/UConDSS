d0c28f0625b8231393974287ea1c432c14df9388
Mathias112 <master19@student.aau.dk>
2022-04-19 10:27:55 +0200

    Update1

--- src/STproject/Controllers/DashboardUconDataVisualizationController.java
+++ src/STproject/Controllers/DashboardUconDataVisualizationController.java
@@ -1,20 +1,79 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package STproject.Controllers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 
+/**
+ * FXML Controller class
+ *
+ * @author mathi
+ */
 public class DashboardUconDataVisualizationController implements Initializable {
 
+    @FXML
+    private ListView<String> listView_Urge;
+    
+    @FXML
+    private ListView<String> listView_TimeLimited;
+    
+    @FXML
+    private TextField average_Urge;
+    
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+        String fileName = "C:\\jar-files\\files\\UConDataLog.txt";
+        File file = new File(fileName);
+
+        try {
+            Scanner inputStream = new Scanner(file);
+
+            while (inputStream.hasNext()) {
+
+                inputStream.useDelimiter("/");
+
+                String data = inputStream.next();
+
+                String[] splitted = data.split("\n");  // "\n" betyder linje skift og muliggør at man kan gemme hver række i et array
+                
+                for (int i = 0; i < splitted.length; i++) {
+
+                    String trimmedData = splitted[i].substring(18, 75);
+                    String trimmedData1 = trimmedData.trim();  // trim() fjerner alle "whitespace" tilsidst i hver String
+
+                    if (trimmedData1.endsWith("MSG_BUTTON_FUNCTION_RELEASED")) {
+                        
+                        listView_Urge.getItems().add(trimmedData1);
+                        
+                        //average_Urge.setText("" + trimmedData1.length());   // Den her tager det forkerte gennemsnit lige nu
+                        average_Urge.setText("" + trimmedData1.length());
+                    }
+                }
+            }
+
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
 
 
-    
 }
--- src/STproject/Main/Main.java
+++ src/STproject/Main/Main.java
@@ -5,37 +5,41 @@
 package STproject.Main;
 
 import STproject.Models.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 public class Main extends Application {
 
     public static PatientsCprList patient = new PatientsCprList();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         launch(args);
     }
 
-
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/ressources/LoginView.fxml")); // /ressources/LoginView.fxml
+        Parent root = FXMLLoader.load(getClass().getResource("/ressources/SearchCreateView.fxml")); // /ressources/LoginView.fxml
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        stage.setTitle("UConDSS");   // satter titel
+        stage.setTitle("UConDSS");   // Sætter titel
         stage.setResizable(false);   // Denne linje gør at man ikke kan udvide skærm manuelt
-        
-        
+
     }
 
     public void readClasses() {
         try {
             //Main.patient = DatabaseHandler.readPatient();
+            
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
