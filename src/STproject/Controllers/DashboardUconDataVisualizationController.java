7813b5972014b465b17eea3d4b32174fbbc22b2f
Mathias112 <master19@student.aau.dk>
2022-04-22 11:06:48 +0200

    o

--- src/STproject/Controllers/DashboardUconDataVisualizationController.java
+++ src/STproject/Controllers/DashboardUconDataVisualizationController.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package STproject.Controllers;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import static java.lang.Math.log;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Scanner;
@@ -17,24 +11,23 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javax.swing.JOptionPane;
 
-/**
- * FXML Controller class
- *
- * @author mathi
- */
 public class DashboardUconDataVisualizationController implements Initializable {
 
     @FXML
     private ListView<String> listView_Urge;
-    
+
     @FXML
     private ListView<String> listView_TimeLimited;
-    
+
     @FXML
     private TextField average_Urge;
-    
-    
-    
+
+    @FXML
+    private TextField average_TimeLimitedSessions;
+
+    @FXML
+    private TextField average_ConstantIntensity;
+
     /**
      * Initializes the controller class.
      */
@@ -55,23 +48,36 @@ public class DashboardUconDataVisualizationController implements Initializable {
 
                 String[] splitted = data.split("\n");  // "\n" betyder linje skift og muliggør at man kan gemme hver række i et array
                 
+                int count_AverageUrge = 0;
+                int count_TimelimitedSessions = 0;
+                
                 for (int i = 0; i < splitted.length; i++) {
 
                     String trimmedData = splitted[i].substring(18, 75);
                     String trimmedData1 = trimmedData.trim();  // trim() fjerner alle "whitespace" tilsidst i hver String
+
+                    String test = splitted[i].substring(18,110);
                     
+                    //if (test.endsWith("s")){
+                        System.out.println(test);
+                    ///}
+
                     if (trimmedData1.endsWith("MSG_BUTTON_FUNCTION_RELEASED")) {
-                        
+                        count_AverageUrge++;
                         listView_Urge.getItems().add(trimmedData1);
-                        
-                        
-                        //average_Urge.setText();      // Den her tager det forkerte gennemsnit lige nu
+
                     }
-                    
-                    
-                    
-                    
+
+                    if (trimmedData1.endsWith("MSG_LOG_THERAPY_STARTED") || trimmedData1.endsWith("MSG_LOG_THERAPY_STOPPED") || trimmedData1.endsWith("MSG_LOG_THERAPY_INTENSITY_CHANGE")) {
+                        count_TimelimitedSessions++;
+                        
+                        listView_TimeLimited.getItems().add(trimmedData1);
+
+                    }
+
                 }
+                average_Urge.setText("" + count_AverageUrge);
+                average_TimeLimitedSessions.setText("" + count_TimelimitedSessions / 2);
             }
 
             inputStream.close();
@@ -79,11 +85,5 @@ public class DashboardUconDataVisualizationController implements Initializable {
             JOptionPane.showMessageDialog(null, e);
         }
     }
-    
-    
-    
-    
-    
-
 
 }
