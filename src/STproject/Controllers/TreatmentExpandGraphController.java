e5d5b1864517f3a51d57b053723b7d3b4d7a3873
Niko Mueller <nmla18@student.aau.dk>
2022-04-29 12:58:20 +0200

    treatmentExpandGraph commit

--- src/STproject/Controllers/TreatmentExpandGraphController.java
+++ src/STproject/Controllers/TreatmentExpandGraphController.java
@@ -1,13 +1,19 @@
 package STproject.Controllers;
 
+import static STproject.Main.Main.patient;
 import static STproject.Main.Main.symptoms;
+import STproject.Models.DatabaseHandler;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
+import javax.swing.JOptionPane;
 
 public class TreatmentExpandGraphController implements Initializable {
 
@@ -16,14 +22,53 @@ public class TreatmentExpandGraphController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        XYChart.Series<String, Integer> seriesPrevious = new XYChart.Series();
-        seriesPrevious.setName("Pre symptoms");
-        seriesPrevious.getData().add(new XYChart.Data("IEs", symptoms.getIEsPerDay()));
-        seriesPrevious.getData().add(new XYChart.Data("UEs", symptoms.getUEsPerDay()));
-        seriesPrevious.getData().add(new XYChart.Data("Urination", symptoms.getUrinationPerDay()));
-        seriesPrevious.getData().add(new XYChart.Data("Nocturia Ep", symptoms.getNocturiaEpisodes()));
-        barChart.setAnimated(false);
+// dummy
+        patient.setCprNumber("0205950254");
 
-        barChart.getData().addAll(seriesPrevious);
+        barChart.setAnimated(false);
+        try {
+            Connection conn = DatabaseHandler.getConnection();
+
+            PreparedStatement psCount = conn.prepareStatement("SELECT COUNT(*) FROM SymptomsBaseline WHERE patientCPR = ?");
+            psCount.setString(1, patient.getCprNumber());
+            ResultSet rsCount = psCount.executeQuery();
+            rsCount.next();
+            int count = rsCount.getInt(1);
+
+            if (count != 0) {
+                for (int i = 1; i <= count; i++) {
+                    System.out.println(i);
+                    PreparedStatement ps = conn.prepareStatement(
+                            "SELECT IEsPerDay, UEsPerDay, UrinationPerDay,"
+                            + " NocturiaEpisodes FROM SymptomsBaseline "
+                            + "WHERE patientCPR = ? AND symptomsIndex = ?");
+                    ps.setString(1, patient.getCprNumber());
+                    ps.setInt(2, i);
+                    ResultSet rs = ps.executeQuery();
+                    rs.next();
+
+                    System.out.println(rs.getInt(1));
+                    System.out.println(rs.getInt(2));
+                    System.out.println(rs.getInt(3));
+                    System.out.println(rs.getInt(4));
+
+                    XYChart.Series<String, Integer> seriesAdd = new XYChart.Series();
+                    seriesAdd.setName("" + i);
+                    seriesAdd.getData().add(new XYChart.Data("IEs", rs.getInt(1)));
+                    seriesAdd.getData().add(new XYChart.Data("UEs", rs.getInt(2)));
+                    seriesAdd.getData().add(new XYChart.Data("Urination", rs.getInt(3)));
+                    seriesAdd.getData().add(new XYChart.Data("Nocturia Ep", rs.getInt(4)));
+
+                    barChart.getData().addAll(seriesAdd);
+
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Error treatmentExpandGraph1");
+            }
+
+        } catch (Exception e) {
+        }
+
     }
 }
