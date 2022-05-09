68469d590a7a6429ad642532e9e3808fd0c9b45b
Niko Mueller <nmla18@student.aau.dk>
2022-05-09 14:02:54 +0200

    Update DashboardTreatmentEvaluationController.java

--- src/STproject/Controllers/DashboardTreatmentEvaluationController.java
+++ src/STproject/Controllers/DashboardTreatmentEvaluationController.java
@@ -9,29 +9,30 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 
 public class DashboardTreatmentEvaluationController implements Initializable {
-
+    
     int usedCorrect;
-
+    
     @FXML
     private TextField textField_TL_MaxIntensity;
-
+    
     @FXML
     private TextField textField_TL_SessionTime;
-
+    
     @FXML
     private TextField textField_Urge_MaxIntensity;
-
+    
     @FXML
     private TextField textField_Urge_SessionTime;
-
+    
     @FXML
     private RadioButton radioBtn_UsedCorrect;
-
+    
     @FXML
     private RadioButton radioBtn_UsedIncorrect;
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Opdater symptoms liste
@@ -40,20 +41,25 @@ public class DashboardTreatmentEvaluationController implements Initializable {
         textField_Urge_SessionTime.setText(Main.treatmentSetting.getUrgeSetting());
         textField_Urge_MaxIntensity.setText("" + Main.treatmentSetting.getUrgeIntensity());
     }
-
+    
     public void clickRadioBtnCorret(ActionEvent event) {
         radioBtn_UsedIncorrect.setSelected(false);
         radioBtn_UsedCorrect.setSelected(true);
         usedCorrect = 1;
     }
-
+    
     public void clickRadioBtnIncorret(ActionEvent event) {
         radioBtn_UsedCorrect.setSelected(false);
         radioBtn_UsedIncorrect.setSelected(true);
         usedCorrect = 0;
     }
-
+    
     public void clickBtnSave() {
-        DatabaseHandler.saveCorrectivenessToDb(usedCorrect);
+        if (!radioBtn_UsedCorrect.isSelected() && !radioBtn_UsedIncorrect.isSelected()) {
+            JOptionPane.showMessageDialog(null, "Select one.");
+        } else {
+            DatabaseHandler.saveCorrectivenessToDb(usedCorrect);
+            JOptionPane.showMessageDialog(null, "Correctiveness score saved.");
+        }
     }
 }
